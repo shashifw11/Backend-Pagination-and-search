0c1e0c21fed02eb454a4cd0bf25ba8cb57e5de17
Shashi Kumar <shekhar.shashi735@gmail.com>
2024-08-18 20:15:59 +0530

    otp send sucessfull in email verification while user creation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const User = require("../models/user.model");
+const sendEmail = require("../utils/sendEmail");
 
  const router = express.Router();
 
  router.post("/" , async (req,res)=>{
     try{
       const user = await User.create(req.body);
+        //const user = new User(req.body);
+        user.otp = "6245"
+        await user.save();
+        var message = {
+            // here we create the massage for sending
+            from: "shekhar.shashi735@gmail.com",
+            to: [user.email], 
+            subject: `${user.otp} Please verify`,
+            text: `Hi your producted is created please verify the otp`,
+            html: "<p>producted is created Please verify</p>",
+            attachments: null,
+          };  
+          sendEmail(message);
       return res.status(200).send(user);
     }catch(err){
     return res.status(500).send(err.message);
@@ -14,9 +28,10 @@ const User = require("../models/user.model");
 
  router.get("/" , async(req,res)=>{
     try{
-
+    const user = await User.find().lean().exec();
+    return res.status(200).send(user);
     }catch(err){
-
+     return res.status(500).send(err.message);
     }
  })
 
